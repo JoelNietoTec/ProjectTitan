23aef45b03c55934b63ae8756d8c54015b11fd44
Joel Nieto <joelnietotec@outlook.com>
2017-05-19 18:48:08 -0500

    individuals forms

---
+++ TitanWebApp/src/app/individuals/individuals-form/individuals-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'individuals-form',
+  templateUrl: './individuals-form.component.html',
+  styleUrls: ['./individuals-form.component.css']
+})
+export class IndividualsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- TitanWebApp/src/app/individuals/individuals.module.ts
+++ TitanWebApp/src/app/individuals/individuals.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
 import { IndividualsComponent } from './individuals.component';
+import { IndividualsFormComponent } from './individuals-form/individuals-form.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, 
+    FormsModule
   ],
-  declarations: [IndividualsComponent]
+  declarations: [IndividualsComponent, IndividualsFormComponent]
 })
 export class IndividualsModule { }
