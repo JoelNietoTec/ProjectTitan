431f017e5cf627b873ac8c36b88f52baf488b73e
Joel Nieto <joelnietotec@outlook.com>
2017-06-13 18:45:41 -0500

    Master

--- TitanDB/Tables/ParamMaster.sql
+++ TitanDB/Tables/ParamMaster.sql
@@ -3,6 +3,6 @@
 	[ID] INT NOT NULL PRIMARY KEY IDENTITY,
     [Name] NVARCHAR(100) NULL, 
     [EnglishName] NVARCHAR(100) NULL, 
-    [FreeField] TINYINT NULL, 
-    [IsRequired] TINYINT NULL 
+    [CreateDate] DATETIME NULL, 
+    [ModificateDate] DATETIME NULL 
 )
--- TitanWebAPI/Models/Params/ParamMaster.cs
+++ TitanWebAPI/Models/Params/ParamMaster.cs
@@ -1,5 +1,6 @@
 namespace TitanWebAPI.Models.Params
 {
+    using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
@@ -21,11 +22,13 @@ namespace TitanWebAPI.Models.Params
         [StringLength(100)]
         public string EnglishName { get; set; }
 
-        public int FreeField { get; set; }
-
-        public int IsRequired { get; set; }
-
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<ParamValue> ParamValues { get; set; }
+
+        [Column(TypeName = "DateTime2")]
+        public DateTime? CreateDate { get; set; }
+
+        [Column(TypeName = "DateTime2")]
+        public DateTime? ModificateDate { get; set; }
     }
 }
