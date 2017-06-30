4651b349bfec3e9e9faf2a06d1e8b0fe94082759
Joel Nieto <joelnietotec@outlook.com>
2017-06-29 20:50:21 -0500

    val

---
+++ TitanDB/Tables/ParamSubValues.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ParamSubValues]
+(
+	[ID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ParamValueID] INT NOT NULL, 
+    [DisplayValue] NVARCHAR(200) NULL, 
+    [EnglishDisplayValue] NVARCHAR(200) NULL, 
+    [Score] NUMERIC(10, 2) NULL 
+)
+
+GO
